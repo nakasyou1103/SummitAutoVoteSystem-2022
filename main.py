4fe0cef807fe554551a9e5414abd40d20c86aa49
nakasyou1103 <79000684+nakasyou1103@users.noreply.github.com>
2022-07-04 17:51:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ def xform(xpath,txt):
     texts.clear()
     texts.send_keys(txt)
 def xclick(xpath):
-    driver.find_element_by_xpath(xpath).click()
+
+    driver.find_element_by_xpath(xpath).click()        
+
+        
+
+    
 
 
 def to(seconds):
@@ -18,22 +23,31 @@ def to(seconds):
     return f"{h:02}:{m:02}:{s:02}"
 def touhu():
     driver.delete_all_cookies()
-    sleep(0.02)
+    sleep(0.03)
     driver.get("https://iwebms.com/cp/summit_sousenkyo/election01.php")
-    sleep(0.02)
+    sleep(0.03)
     xclick('//*[@id="election"]/div[2]/ul[1]/li[7]/a')
-    sleep(0.02)
+    sleep(0.03)
     xclick('//*[@id="vote"]')
-    sleep(0.02)
+    sleep(0.03)
 def ringo():
     driver.delete_all_cookies()
-    sleep(0.02)
+    sleep(0.03)
     driver.get("https://iwebms.com/cp/summit_sousenkyo/election02.php")
-    sleep(0.02)
+    sleep(0.03)
     xclick('//*[@id="election"]/div[2]/ul[1]/li[2]/a')
-    sleep(0.02)
+    sleep(0.03)
     xclick('//*[@id="vote"]')
-    sleep(0.02)
+    sleep(0.03)
+def wagasi():
+    driver.delete_all_cookies()
+    sleep(0)
+    driver.get("https://iwebms.com/cp/summit_sousenkyo/election03.php")
+    sleep(0.03)
+    xclick('//*[@id="election"]/div[2]/ul/li[3]/a')
+    sleep(0.03)
+    xclick('//*[@id="vote"]')
+    sleep(0.03)   
 def nokori(ind,maxed):
     e = str(round(ind/maxed*100,2))
     nokor = maxed - ind
@@ -43,15 +57,20 @@ def nokori(ind,maxed):
     if byo == 0:
         byo = 0.1
     ikai = byo/(int(ind)+1)
+    errri = "エラー率:"+str(round(err/(int(ind)+1)*100,2))+"%"
     byosoku = str(round(1/ikai,2))+"票/s"
     nokobyo = "残り約"+to(ikai*nokor)+"s"
-    return [ind+"/"+maxed,byosoku,e+"%",nokobyo]
-
+    return [ind+"/"+maxed,byosoku,e+"%",errri,nokobyo]
+err = 0
 start = time()
 l = int(input("数量"))
 driver = webdriver.Chrome()
 for i in range(l):
     print(" ".join(nokori(i,l)))
     #print(i+1)
-    ringo()
-    touhu()
+    try:
+        ringo()
+        touhu()
+    except:
+        err += 1
+        print("er")
